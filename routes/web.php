f725813ba26eca21c46b80ca1832cea8a7193522
PebryAjeng <105222031@student.universitaspertamina.ac.id>
2024-12-11 10:27:36 +0700

    revisi 4

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,49 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Signin Routes
+Route::get('/signin', function () {
+    return 'Signin Form';
+});
+
+Route::post('/signin', function () {
+    return 'Process Signin';
+});
+
+// Signup Routes
+Route::get('/signup', function () {
+    return 'Signup Form';
+});
+
+Route::post('/signup', function () {
+    return 'Process Signup';
+});
+
+// Home Route
 Route::get('/', function () {
-    return view('welcome');
+    return 'Home Page';
+});
+
+// Blog Routes
+Route::get('/blog', function () {
+    return 'Blog List';
+});
+
+Route::get('/blog/{slug}', function ($slug) {
+    return "Blog Detail: $slug";
+});
+
+// Category Route
+Route::get('/category/{slug}', function ($slug) {
+    return "Category: $slug";
+});
+
+// Author Route
+Route::get('/author/{username}', function ($username) {
+    return "Author: $username";
+});
+
+// Privacy Policy Route
+Route::get('/privacy-policy', function () {
+    return 'Privacy Policy Page';
 });
